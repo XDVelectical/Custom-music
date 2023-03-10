97fa4b852c7faf8aa5bbfc4b9ae4df6a70c42e27
Velectical <106909222+XDVelectical@users.noreply.github.com>
2023-03-10 10:55:45 +0100

    Patch 2 (#5)
    * Update GUI.java
    
    * Update GUI.java
    
    ---------
    
    Co-authored-by: Velecticall <122977016+Velecticall@users.noreply.github.com>

--- GUI.java
+++ GUI.java
@@ -1,31 +1,6 @@
-/*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.jagrosh.jmusicbot.gui;
-
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
-import javax.swing.WindowConstants;
-import com.jagrosh.jmusicbot.Bot;
-
 
 /**
  *
- * @author John Grosh <john.a.grosh@gmail.com>
  */
 public class GUI extends JFrame 
 {
@@ -34,6 +9,8 @@ public class GUI extends JFrame
     
     public GUI(Bot bot) 
     {
+
+
         super();
         this.bot = bot;
         console = new ConsolePanel();
@@ -51,13 +28,7 @@ public class GUI extends JFrame
         setVisible(true);
         addWindowListener(new WindowListener() 
         {
-            @Override public void windowOpened(WindowEvent e) { /* unused */ }
-            @Override public void windowClosing(WindowEvent e) 
-            {
-                try
-                {
-                    bot.shutdown();
-                }
+          
                 catch(Exception ex)
                 {
                     System.exit(0);
