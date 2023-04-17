00a670914d6e25b85860cd2183e9828721639693
Velectical <106909222+XDVelectical@users.noreply.github.com>
2023-04-17 10:02:31 +0200

    Create gui-loader.java

---
+++ gui-loader.java
@@ -0,0 +1,164 @@
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        if(args.length > 0)
+            switch(args[0].toLowerCase())
+            {
+                case "generate-config":
+                    BotConfig.writeDefaultConfig();
+                    return;
+                default:
+            }
+        startBot();
+    }
+    
+    private static void startBot()
+    {
+        // create prompt to handle startup
+        Prompt prompt = new Prompt("JMusicBot");
+        
+        // startup checks
+        OtherUtil.checkVersion(prompt);
+        OtherUtil.checkJavaVersion(prompt);
+        
+        // load config
+        BotConfig config = new BotConfig(prompt);
+        config.load();
+        if(!config.isValid())
+            return;
+        LOG.info("Loaded config from " + config.getConfigLocation());
+        
+        // set up the listener
+        EventWaiter waiter = new EventWaiter();
+        SettingsManager settings = new SettingsManager();
+        Bot bot = new Bot(waiter, config, settings);
+        
+        AboutCommand aboutCommand = new AboutCommand(Color.BLUE.brighter(),
+                                "a music bot that is [easy to host yourself!](https://github.com/jagrosh/MusicBot) (v" + OtherUtil.getCurrentVersion() + ")",
+                                new String[]{"High-quality music playback", "FairQueue™ Technology", "Easy to host yourself"},
+                                RECOMMENDED_PERMS);
+        aboutCommand.setIsAuthor(false);
+        aboutCommand.setReplacementCharacter("\uD83C\uDFB6"); // 🎶
+        
+        // set up the command client
+        CommandClientBuilder cb = new CommandClientBuilder()
+                .setPrefix(config.getPrefix())
+                .setAlternativePrefix(config.getAltPrefix())
+                .setOwnerId(Long.toString(config.getOwnerId()))
+                .setEmojis(config.getSuccess(), config.getWarning(), config.getError())
+                .setHelpWord(config.getHelp())
+                .setLinkedCacheSize(200)
+                .setGuildSettingsManager(settings)
+                .addCommands(aboutCommand,
+                        new PingCommand(),
+                        new SettingsCmd(bot),
+                        
+                        new LyricsCmd(bot),
+                        new NowplayingCmd(bot),
+                        new PlayCmd(bot),
+                        new PlaylistsCmd(bot),
+                        new QueueCmd(bot),
+                        new RemoveCmd(bot),
+                        new SearchCmd(bot),
+                        new SCSearchCmd(bot),
+                        new ShuffleCmd(bot),
+                        new SkipCmd(bot),
+
+                        new ForceRemoveCmd(bot),
+                        new ForceskipCmd(bot),
+                        new MoveTrackCmd(bot),
+                        new PauseCmd(bot),
+                        new PlaynextCmd(bot),
+                        new RepeatCmd(bot),
+                        new SkiptoCmd(bot),
+                        new StopCmd(bot),
+                        new VolumeCmd(bot),
+                        
+                        new PrefixCmd(bot),
+                        new SetdjCmd(bot),
+                        new SkipratioCmd(bot),
+                        new SettcCmd(bot),
+                        new SetvcCmd(bot),
+                        
+                        new AutoplaylistCmd(bot),
+                        new DebugCmd(bot),
+                        new PlaylistCmd(bot),
+                        new SetavatarCmd(bot),
+                        new SetgameCmd(bot),
+                        new SetnameCmd(bot),
+                        new SetstatusCmd(bot),
+                        new ShutdownCmd(bot)
+                );
+        if(config.useEval())
+            cb.addCommand(new EvalCmd(bot));
+        boolean nogame = false;
+        if(config.getStatus()!=OnlineStatus.UNKNOWN)
+            cb.setStatus(config.getStatus());
+        if(config.getGame()==null)
+            cb.useDefaultGame();
+        else if(config.getGame().getName().equalsIgnoreCase("none"))
+        {
+            cb.setActivity(null);
+            nogame = true;
+        }
+        else
+            cb.setActivity(config.getGame());
+        
+        if(!prompt.isNoGUI())
+        {
+            try 
+            {
+                GUI gui = new GUI(bot);
+                bot.setGUI(gui);
+                gui.init();
+            } 
+            catch(Exception e) 
+            {
+                LOG.error("Could not start GUI. If you are "
+                        + "running on a server or in a location where you cannot display a "
+                        + "window, please run in nogui mode using the -Dnogui=true flag.");
+            }
+        }
+        
+        // attempt to log in and start
+        try
+        {
+            JDA jda = JDABuilder.create(config.getToken(), Arrays.asList(INTENTS))
+                    .enableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+                    .disableCache(CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS, CacheFlag.EMOTE, CacheFlag.ONLINE_STATUS)
+                    .setActivity(nogame ? null : Activity.playing("loading..."))
+                    .setStatus(config.getStatus()==OnlineStatus.INVISIBLE || config.getStatus()==OnlineStatus.OFFLINE 
+                            ? OnlineStatus.INVISIBLE : OnlineStatus.DO_NOT_DISTURB)
+                    .addEventListeners(cb.build(), waiter, new Listener(bot))
+                    .setBulkDeleteSplittingEnabled(true)
+                    .build();
+            bot.setJDA(jda);
+        }
+        catch (LoginException ex)
+        {
+            prompt.alert(Prompt.Level.ERROR, "JMusicBot", ex + "\nPlease make sure you are "
+                    + "editing the correct config.txt file, and that you have used the "
+                    + "correct token (not the 'secret'!)\nConfig Location: " + config.getConfigLocation());
+            System.exit(1);
+        }
+        catch(IllegalArgumentException ex)
+        {
+            prompt.alert(Prompt.Level.ERROR, "JMusicBot", "Some aspect of the configuration is "
+                    + "invalid: " + ex + "\nConfig Location: " + config.getConfigLocation());
+            System.exit(1);
+        }
+    }
+}
+	
+
+	new string:create ultimate;S'run configuration' 
+	[/on start initialise]{If:error null}['display:,'error message'
+	Prompt.alert(Prompt.Level.ERROR, "JMusicBot", ex + "\nPlease make sure you are "
+                    + "editing the correct config.txt file, and that you have used the "
+                    + "correct token (not the 'secret'!)\nConfig Location: " + config.getConfigLocation());
+            System.exit(1);
+
+	Open.alert(prompt.level.error303, "compile error found restarting process", exe. +"\nPlease alert administrators"
+	
